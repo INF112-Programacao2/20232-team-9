51b94f4a17a1b879f76b592294d7b010d5693314
Nicolas Reis <nicolas.e.silva@ufv.br>
2023-10-20 17:44:12 -0300

    Comentários e Inicialização do Trabalho

--- CONTABILIDADE FISICA/contabilidade_fisica.cpp
+++ CONTABILIDADE FISICA/contabilidade_fisica.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+//declaração de imposto de renda]
+//inss e imposto de renda
+//tetos de alíquotas
+//buscar informações no site da receita federal
+//caluculo de alíquotas de imposto de renda mensal e anual
+//deduação por dependente
+//desconto simplificado
+//cálculos básicos por porcentagem das tabelas
--- CONTABILIDADE JURIDICA/contabilidade_juridica.cpp
+++ CONTABILIDADE JURIDICA/contabilidade_juridica.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+//tributação federal:
+//tributação estadual:
+//tributação por municipio:
+//CNAE sob tipo de empresa (qual atuação no ramo)
+//Razão social : nome da empresa
+//apelido : nome fantasia
+//NATUREZA JURIDICA (ja vem no cnpj)
+//IPI (depende do ramo)
+//Verificar se tem matriz, filial ou franquia
+//dados basicos sobre a empresa
+//PLANOS DE CONTAS PARA CADA MODELO DE N    EGÓCIO
+//PLANO DE CONTAS É O BALANÇO
+//BALANÇO (feito após feito plano de contas, podendo ser mensal, trimestral, semestral ou anual)
+//o balanço será feito com  vetores (para guardar datas) com double (dinheiro) e deve ser fechado (as contas devem bater)
+// a tributação é totalmente feita pelo CNAE 
+//LUCRO REAL, SIMPLES NACIONAL, MICROEMPRESA, ETC (formas de tributação)
+
