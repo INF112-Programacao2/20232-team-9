16e2c06a38a5d5d25341320375f3f4a0f6394bc0
Hugo Araujo <hugo.costa@ufv.br>
2023-10-24 18:26:27 -0300

    Criação da classe Cadastro

--- CADASTRO/cadastro.cpp
+++ CADASTRO/cadastro.cpp
@@ -1,1 +1,103 @@
 #include <iostream>
+#include <cstring>
+#include "cadastro.h"
+
+
+Cadastro::Cadastro(char *nomepessoa, char *nomeempresa, char *apelidoempresa, char *local, char *cnpj){
+    
+    strcpy (_nomepessoa, nomepessoa);
+    strcpy (_nomeempresa, nomeempresa);
+    strcpy (_apelidoempresa, apelidoempresa);
+    strcpy (_local, local);
+    strcpy (_cnpj, cnpj);
+
+}
+
+
+Cadastro::Cadastro (char *nomepessoa, char *local, char *cpf){
+
+    strcpy (_nomepessoa, nomepessoa);
+    strcpy (_local, local);
+    strcpy (_cpf, cpf);
+
+}
+
+
+char Cadastro::get_nomepessoa (){
+
+    return *_nomepessoa;
+
+}
+
+
+char Cadastro::get_nomeempresa (){
+
+    return *_nomeempresa;
+
+}
+
+
+char Cadastro::get_apelidoempresa(){
+
+    return *_apelidoempresa;
+
+}
+
+
+char Cadastro::get_cnpj(){
+
+    return *_cnpj;
+
+}
+
+
+char Cadastro::get_cpf(){
+
+    return *_cpf;
+
+}
+
+
+char Cadastro::get_local(){
+
+    return *_local;
+
+}
+
+
+void Cadastro::set_nomepessoa (char *nomepessoa){
+
+    strcpy (_nomepessoa, nomepessoa);
+
+}
+
+
+void Cadastro::set_nomeempresa (char *nomeempresa){
+
+    strcpy (_nomeempresa, nomeempresa);
+
+}
+
+void Cadastro::set_apelidoempresa (char *apelidoempresa){
+
+    strcpy (_apelidoempresa, apelidoempresa);
+
+}
+
+void Cadastro::set_local (char *local){
+
+    strcpy (_local, local);
+
+}
+
+void Cadastro::set_cpf (char *cpf){
+
+    strcpy (_cpf, cpf);
+
+}
+
+void Cadastro::set_cnpj (char *cnpj){
+
+    strcpy (_cnpj, cnpj);
+
+}
--- CADASTRO/cadastro.h
+++ CADASTRO/cadastro.h
@@ -1,1 +1,45 @@
-#include <iostream>
+class Cadastro{
+ 
+ private:
+  
+  char _nomepessoa [50];
+  char _nomeempresa [30];
+  char _apelidoempresa [30];
+  char _local [30];
+  char _cpf [11];
+  char _cnpj [14];
+
+ 
+ 
+ public:
+
+    Cadastro (char *nomepessoa, char *nomeempresa, char *apelidoempresa, char *local, char *cnpj);
+
+    Cadastro (char *nomepessoa, char *local, char *cpf);
+
+    char get_nomepessoa ();
+
+    char get_nomeempresa ();
+
+    char get_apelidoempresa ();
+
+    char get_local ();
+
+    char get_cpf ();
+
+    char get_cnpj ();
+
+    void set_nomepessoa (char *nomepessoa);
+
+    void set_nomeempresa (char *nomeempresa);
+
+    void set_apelidoempresa (char *apelidoempresa);
+
+    void set_local (char *local);
+
+    void set_cpf (char *cpf);
+
+    void set_cnpj (char *cnpj);
+
+
+};
