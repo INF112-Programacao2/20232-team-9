5a86da086bd487038d25666c5a9fdf9cdbf84b38
Hugo Araujo <hugo.costa@ufv.br>
2023-11-21 17:06:46 -0300

    Mudei e adicionei classes e alterei a main

--- CADASTRO/main.cpp
+++ CADASTRO/main.cpp
@@ -1,73 +1,59 @@
 #include <iostream>
 #include <cstring>
-#include "cadastro.h"
+#include "pessoa.h"
+#include "pessoajuridica.h"
 
-std::string confere_nomes (std::string &nome){
- 
- int contnotalpha;
- int cont=1;
-  
-  while(cont>0){
+void confere_nomes (std::string &nome){
 
-   contnotalpha=0;
-
-    for (int i=0; nome[i] != '\0'; i++){
-       
+  while(true){
+    try{
+      for (int i=0; nome[i] != '\0'; i++){
         if((nome.at(i) == ' ') && i != 0){
             continue;
         }
-
         else if(!isalpha (nome[i])){
+          throw std::invalid_argument ("Nome inválido!! O nome deve conter apenas letras! Digite novamente:");
+          break;
+         }
+       }
 
-            contnotalpha++;
+       break;
+      }catch (std::invalid_argument &e){
+        std::cout<<e.what()<<std::endl;
+        getline(std::cin,nome);
+      }   
+  }
 
-        }
-    }
-
-
-    if(contnotalpha>0){
-
-         std::cout<<"Nome inválido!! O nome deve conter apenas letras! \n";
-         std::cout<<"Digite um nome válido: \n";
-         getline (std::cin, nome);
-
-    }
-
-    else {
-      break;
-    }
-
-  }   
-
- return nome; 
-   
 }
 
 
 int main(){
 
-    std::string nomepessoa, nomeempresa, apelidoempresa, local, cnpj, cpf;
-    int opcao, confirmacao, selecionador;
+    std::string nomepessoa, nomeempresa, apelidoempresa, local, cnpj, cpf, opcao;
+    int confirmacao, selecionador;
 
     std::cout<<"Olá cliente! \n";
     std::cout<<"Deseja realizar qual tipo de contabilidade? \n";
     std::cout<<"1- Contabilidade Jurídica. \n";
     std::cout<<"2- Contabilidade Física. \n";
-    std::cout<<"Digite 1 para Contabilidade Jurídica ou 2 para Contabilidade Física: \n";
-    std::cin>>opcao;
-
-
-    while (opcao != 1 && opcao != 2){
-
-        std::cerr<<"Opção inválida!! Digite 1 para Contabilidade Jurídica ou 2 para Contabilidade Física: \n";
+    std::cout<<"Digite 1 para Contabilidade Jurídica ou 2 para Contabilidade Física: \n";   
+    while(true){
+      try{
         std::cin>>opcao;
+        if(opcao != "1" && opcao != "2"){
+          throw std::invalid_argument ("Opção inválida!! Digite 1 para Contabilidade Jurídica ou 2 para Contabilidade Física:");
+        }
 
+        break;
+
+      }catch(std::invalid_argument &e){
+        std::cerr<<e.what()<<std::endl;
+      }
     }
 
+    if(opcao=="1"){
 
-
-    if(opcao==1){
-
+        std::cout<<std::endl;
         std::cout<<"Bem-vindo, cliente! \n";
         std::cout<<"Iremos iniciar o cadastro. \n";
         std::cout<<"Digite o nome completo do proprietário da empresa: \n";
@@ -76,144 +62,91 @@ int main(){
         confere_nomes(nomepessoa);
         std::cout<<"Digite o nome da empresa: \n";
         getline (std::cin, nomeempresa);
-        confere_nomes(nomeempresa);
         std::cout<<"Digite o apelido da empresa: \n";
         getline (std::cin, apelidoempresa);
-        std::cout<<"Digite o estado em que a empresa encontra-se localizada: \n";
-        getline (std::cin, local);
-        confere_nomes(local);
+        std::cout<<"Digite o número correpondente ao estado em que a empresa encontra-se localizada: \n";
+        std::cout<<"(1)- MG \n"<<"(2)- SP \n"<<"(3)- RJ \n";
+        while(true){
+          try{
+            std::cin>>opcao;
+              if(opcao != "1" && opcao != "2" && opcao != "3"){
+                throw std::invalid_argument ("Opção inválida!! Digite uma opção válida: ");
+              }
 
-        Cadastro juridico (nomepessoa, nomeempresa, apelidoempresa, local);
+            break;
+          }catch(std::invalid_argument &e){
+            std::cerr<<e.what()<<std::endl;
+          }
+        }
+        if(opcao=="1"){
+          local="MG";
+        }
+        else if(opcao=="2"){
+          local="SP";
+        }
+        else{
+          local="RJ";
+        }
+
+        PessoaJuridica juridico (nomepessoa, nomeempresa, apelidoempresa, local);
         std::cout<<"Digite o CNPJ do proprietario: \n";
-        std::cin>>cnpj;
-        juridico.confere_cnpj(cnpj);
-        std::cout<<juridico.get_nomepessoa()<<" "<<juridico.get_cnpj()<<" "<<juridico.get_nomeempresa()<<" "<<juridico.get_apelidoempresa()<<" "<<juridico.get_local();
+        juridico.insere_cnpj( );
+        std::cout<<"Digite o CPF do proprietario: \n";
+        juridico.insere_cpf ( );
         std::cout<<std::endl;
-
-        std::cout<<"Você confirma os dados inseridos no cadastro? \n";
-        std::cout<<"1- Sim. \n";
-        std::cout<<"2- Não. \n";
-        std::cout<<"Digite 1 para SIM ou 2 para NÃO: \n";
-        std::cin>>confirmacao;
-       
-
-        while (confirmacao != 1 && confirmacao != 2){
-
-          std::cout<<"Opção inválida!! Digite 1 para SIM ou 2 para NÃO: \n";
-          std::cin>>confirmacao;
-
+        if(juridico.confirma_cadastro (juridico) == true){
+          std::cout<<"Cadastro realizado com sucesso! Obrigado!! \n";
         }
-
-
-
-        if(confirmacao==1){
-
-            std::cout<<"Cadastro realizado com sucesso! Obrigado!! \n";
-
-        }
-
-
-
-        else if (confirmacao==2){
-
-          while (confirmacao=2){
-
-            std::cout<<"Qual dado deseja alterar? \n";
-            std::cout<<"1- Nome do proprietário (DIGITE 1) \n";
-            std::cout<<"2- Nome da empresa (DIGITE 2) \n";
-            std::cout<<"3- Apelido da empresa (DIGITE 3) \n";
-            std::cout<<"4- Localidade (DIGITE 4) \n";
-            std::cout<<"5- CPF (DIGITE 5) \n";
-
-            std::cin>>selecionador;
-
-
-            while (selecionador != 1 && selecionador != 2 && selecionador != 3 && selecionador != 4 && selecionador != 5){
-
-              std::cout<<"Opção inválida!! Digite uma opção válida: \n";
-              std::cin>>selecionador;
-
-            }
-
-            
-            std::cin.ignore();
-
-
-            switch (selecionador){
-
-                case (1):
-
-                  std::cout<<"Digite o novo nome do proprietário: \n";
-
-                  getline (std::cin, nomepessoa); 
-                  confere_nomes(nomepessoa);
-                  juridico.set_nomepessoa(nomepessoa);
-                  break;
-
-                case (2):
-
-                  std::cout<<"Digite o novo nome da empresa: \n";
-                  getline (std::cin, nomeempresa); 
-                  confere_nomes(nomeempresa);
-                  juridico.set_nomeempresa(nomeempresa);
-                  break;
-
-
-                case (3):
-
-                  std::cout<<"Digite o novo apelido da empresa: \n";
-                  getline (std::cin, apelidoempresa); 
-                  juridico.set_apelidoempresa(apelidoempresa);
-                  break;
-
-
-                case(4):
-
-                  std::cout<<"Digite a nova localidade da empresa: \n";
-                  getline (std::cin, local); 
-                  confere_nomes(local);
-                  juridico.set_local(local);
-                  break;
-
-
-                case(5): 
-
-                  std::cout<<"Digite o novo CNPJ: \n";
-                  std::cin>>cnpj; 
-                  juridico.confere_cnpj (cnpj);
-                  break;
-
-            }
-
-
-             std::cout<<juridico.get_nomepessoa()<<" "<<juridico.get_cnpj()<<" "<<juridico.get_nomeempresa()<<" "<<juridico.get_apelidoempresa()<<" "<<juridico.get_local();
-             std::cout<<std::endl;
-             std::cout<<"Você confirma os novos dados inseridos no cadastro? \n";
-             std::cout<<"1- Sim. \n";
-             std::cout<<"2- Não. \n";
-             std::cout<<"Digite 1 para SIM ou 2 para NÃO: \n";
-             std::cin>>confirmacao;
-
-        
-            while (confirmacao != 1 && confirmacao != 2){
-
-              std::cout<<"Opção inválida!! Digite 1 para SIM ou 2 para NÃO: \n";
-              std::cin>>confirmacao;
-
-           }
-
-
-              if(confirmacao==1){
-                 std::cout<<"Cadastro realizado com sucesso! Obrigado!! \n";
-                 break;
-             }
-
+        else{
+          juridico.edicao_cadastro (juridico);
         }
 
     }
 
- }
+    else if(opcao=="2"){
+
+        std::cout<<std::endl;
+        std::cout<<"Bem-vindo, cliente! \n";
+        std::cout<<"Iremos iniciar o cadastro. \n";
+        std::cout<<"Digite seu nome completo: \n";
+        std::cin.ignore();
+        getline (std::cin, nomepessoa);
+        confere_nomes(nomepessoa);
+        std::cout<<"Digite o número correpondente ao seu estado de residência: \n";
+        std::cout<<"(1)- MG \n"<<"(2)- SP \n"<<"(3)- RJ \n";
+        while(true){
+          try{
+            std::cin>>opcao;
+              if(opcao != "1" && opcao != "2" && opcao != "3"){
+                throw std::invalid_argument ("Opção inválida!! Digite uma opção válida: ");
+              }
+
+            break;
+          }catch(std::invalid_argument &e){
+            std::cerr<<e.what()<<std::endl;
+          }
+        }
+        if(opcao=="1"){
+          local="MG";
+        }
+        else if(opcao=="2"){
+          local="SP";
+        }
+        else{
+          local="RJ";
+        }
+
+        Pessoa fisico (nomepessoa, local);
+        std::cout<<"Digite o CPF do proprietario: \n";
+        fisico.insere_cpf ( );
+        std::cout<<std::endl;
+        if(fisico.confirma_cadastro_fis (fisico) == true){
+          std::cout<<"Cadastro realizado com sucesso! Obrigado!! \n";
+        }
+        else{
+          fisico.edicao_cadastro_fis (fisico);
+        }
+    }
 
  return 0;
-
 }
---
+++ CADASTRO/pessoa.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <cstring>
+#include "pessoa.h"
+
+
+Pessoa::Pessoa (std::string nomepessoa, std::string local):
+
+_nomepessoa(nomepessoa), _local(local) {}
+
+
+
+void Pessoa::insere_cpf ( ) {
+   
+    while (true){
+     try{ 
+      std::cin>>_cpf;
+
+        if(_cpf.size()<11 || _cpf.size()>11){
+          throw std::out_of_range ("CPF Inválido!! Digite um novo CPF com tamanho válido:");
+        }
+
+        else{
+          for(int i=0; _cpf[i] != '\0'; i++){
+              if (!isdigit (_cpf[i])){
+                throw std::invalid_argument ("CPF Inválido!! Digite um novo CPF apenas com números:");
+                break;
+              }
+          }
+        }
+
+        break;
+
+     }catch (std::invalid_argument &e){
+        std::cerr<<e.what()<<std::endl;
+
+     }catch (std::out_of_range &e2){
+        std::cerr<<e2.what()<<std::endl;
+     }    
+
+  }
+
+}
+
+void Pessoa::edicao_cadastro_fis (Pessoa &fisico){
+    std::string opcao;
+    bool averigua=false;
+    int selecionador;
+
+          while (averigua==false){
+            std::cout<<std::endl;
+            std::cout<<"Qual dado deseja alterar? \n";
+            std::cout<<"1- Nome do proprietário (DIGITE 1) \n";
+            std::cout<<"2- Localidade (DIGITE 2) \n";
+            std::cout<<"3- CPF (DIGITE 3) \n";
+            while(true){
+              try{
+                std::cin>>opcao;
+
+                if(opcao != "1" && opcao != "2" && opcao != "3"){
+                 throw std::invalid_argument ("Opção inválida!! Digite uma opção válida:");
+                }
+                else{
+                  selecionador=stoi(opcao);
+                  break;
+               }
+
+              }catch(std::invalid_argument &e){
+                std::cerr<<e.what()<<std::endl;
+                }
+            }
+            
+            switch (selecionador){
+                case (1):
+                
+                  std::cout<<"Digite o novo nome que deseja cadastrar: \n";
+                  while(true){
+                    try{
+                      std::cin.ignore ();
+                      getline(std::cin,_nomepessoa);
+                       for (int i=0; _nomepessoa[i] != '\0'; i++){
+                          if((_nomepessoa.at(i) == ' ') && i != 0){
+                            continue;
+                          }
+                          else if(!isalpha (_nomepessoa[i])){
+                            throw std::invalid_argument ("Nome inválido!! O nome deve conter apenas letras! Digite novamente:");
+                            break;
+                          }
+                        }
+
+                        break;
+                     }catch (std::invalid_argument &e){
+                        std::cout<<e.what()<<std::endl;
+                      }   
+                  }
+                  break;
+
+                case(2):
+
+                  std::cout<<"Digite o número correpondente ao novo estado de residência: \n";
+                  std::cout<<"(1)- MG \n"<<"(2)- SP \n"<<"(3)- RJ \n";
+                  while(true){
+                    try{
+                      std::cin>>opcao;
+                      if(opcao != "1" && opcao != "2" && opcao != "3"){
+                        throw std::invalid_argument ("Opção inválida!! Digite uma opção válida: ");
+                      }
+
+                      break;
+                     }catch(std::invalid_argument &e){
+                        std::cerr<<e.what()<<std::endl;
+                      }
+                  }
+                  if(opcao=="1"){
+                    _local="MG";
+                  }
+                  else if(opcao=="2"){
+                   _local="SP";
+                  }
+                  else{
+                   _local="RJ";
+                  }
+                  break;
+
+                case(3): 
+
+                  std::cout<<"Digite o novo CPF: \n";
+                  fisico.insere_cpf ( );
+                  break;  
+
+            }
+                averigua=fisico.confirma_cadastro_fis (fisico);
+
+                if(averigua==true){
+                  std::cout<<"Cadastro realizado com sucesso! Obrigado!! \n";
+                }
+        }
+    }
+
+
+
+bool Pessoa::confirma_cadastro_fis (Pessoa &fisico){
+  
+  std::string opcao;
+
+    std::cout<<std::endl;
+    std::cout<<"NOME: "<<fisico.get_nomepessoa()<<" | CPF: "<<fisico.get_cpf()<<" | LOCALIDADE: "<<fisico.get_local();
+    std::cout<<std::endl;
+    std::cout<<"Você confirma os dados inseridos no cadastro? \n";
+    std::cout<<"1- Sim. \n";
+    std::cout<<"2- Não. \n";
+    std::cout<<"Digite 1 para SIM ou 2 para NÃO: \n";
+     while(true){
+       try{
+        std::cin>>opcao;
+
+          if(opcao != "1" && opcao != "2"){
+            throw std::invalid_argument ("Opção inválida!! Digite 1 para SIM ou 2 para NÃO:");
+          }
+            break;
+        }catch(std::invalid_argument &e){
+          std::cerr<<e.what()<<std::endl;
+        }
+       }
+    
+    if(opcao=="1"){
+      return true;
+    }
+    else{
+      return false;
+    }   
+
+}
+
+
+std::string Pessoa::get_nomepessoa (){
+
+    return _nomepessoa;
+
+}
+
+
+std::string Pessoa::get_cpf(){
+
+    return _cpf;
+
+}
+
+
+std::string Pessoa::get_local(){
+
+    return _local;
+
+}
+
+
+void Pessoa::set_nomepessoa (std::string nomepessoa){
+
+    _nomepessoa = nomepessoa;
+
+}
+
+
+void Pessoa::set_local (std::string local){
+
+    _local = local;
+
+}
+
